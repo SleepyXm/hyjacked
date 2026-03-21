e986497220c21c88a5cae1b3110fd1a8b0ef44dd
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-03-21 03:34:08 +0000

    Update main.py

--- src/app/backend/main.py
+++ src/app/backend/main.py
@@ -8,11 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-<<<<<<< Updated upstream
     allow_origins=["http://localhost:3000"],
-=======
     allow_origins=["http://localhost:3001", "http://localhost:3000"],
->>>>>>> Stashed changes
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
